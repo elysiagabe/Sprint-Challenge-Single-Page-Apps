024a7c309709ebc4e66c562c6f8ba0e451de6cb2
Elysia Gabe <elysiagabe@gmail.com>
2020-02-28 10:05:13 -0800

    Stretch: add details view with modal

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,8 +1,28 @@
-import React from "react";
-import { Col, Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import React, { useState } from "react";
+import { 
+  Col, 
+  Card, 
+  CardImg, 
+  CardText, 
+  CardBody, 
+  CardTitle, 
+  Button,
+  Modal, 
+  ModalHeader, 
+  ModalBody 
+} from 'reactstrap';
 
 export default function CharacterCard(props) {
   console.log("CharacterCard props:", props)
+
+  const {
+    buttonLabel,
+    className
+  } = props;
+
+  const [modal, setModal] = useState(false);
+
+  const toggle = () => setModal(!modal);
 
   return (
     <Col xs="6" md="4">
@@ -10,7 +30,14 @@ export default function CharacterCard(props) {
         <CardImg top width="100%" src={props.imgSrc} alt={props.name}/>
         <CardBody>
           <CardTitle>Name: {props.name}</CardTitle>
-          <CardText>{props.name} is a {props.gender} {props.species} originally from {props.origin}. They are currently {props.status.toLowerCase()} and residing on/in {props.location}.</CardText>
+          <Button color="danger" onClick={toggle}>{buttonLabel} Learn More</Button>
+            <Modal isOpen={modal} toggle={toggle} className={className}>
+              <ModalHeader toggle={toggle}>{props.name}</ModalHeader>
+              <ModalBody>
+                {props.name} is a {props.gender} {props.species} originally from {props.origin}. They are currently {props.status.toLowerCase()} and residing on/in {props.location}.
+              </ModalBody>
+            </Modal>
+          <CardText></CardText>
         </CardBody>
       </Card>
     </Col>
