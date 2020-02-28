4dfb03335c1df8c20db2b458e8cdcc84bde5aa96
Elysia Gabe <elysiagabe@gmail.com>
2020-02-28 08:57:30 -0800

    Updates App.js with Routes and Header.js with NavLinks

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
+import { Route } from "react-router-dom";
 import Header from "./components/Header.js";
+import WelcomePage from "./components/WelcomePage";
 import CharacterList from "./components/CharacterList";
 
 export default function App() {
   return (
     <main>
       <Header />
-      <CharacterList />
+      <Route exact path="/">
+        <WelcomePage />
+      </Route>
+      <Route path="/characters">
+        <CharacterList />
+      </Route>
     </main>
   );
 }
