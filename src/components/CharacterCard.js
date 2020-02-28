105a4cc8fb74dad848f1ebe1e08c50b8cc03d8cc
Elysia Gabe <elysiagabe@gmail.com>
2020-02-28 08:44:37 -0800

    Fetches and renders data pulled from Rick & Morty API

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,19 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  console.log("CharacterCard props:", props)
+
+  return (
+    <section key={props.id}>
+      <img src={props.imgSrc} alt="rick and morty character" />
+      <h3>Name: {props.name}</h3>
+      {/* <p>Status: {props.status}</p>
+      <p>Species: {props.species}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Maybe Origin: {props.origin}</p>
+      <p>Maybe Current Location: {props.location}</p> */}
+
+  <p>{props.name} is a {props.gender} {props.species} originally from {props.origin}. They are currently {props.status.toLowerCase()} and residing on/in {props.location}.</p>
+    </section>
+  );
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,16 +1,40 @@
 import React, { useEffect, useState } from "react";
+import axios from "axios";
+import CharacterCard from "./CharacterCard";
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [characters, setCharacters] = useState([])
+
+  // Set query data later
 
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+    axios.get("https://cors-anywhere.herokuapp.com/https://rickandmortyapi.com/api/character/")
+    .then(response => {
+      //console.log(response.data.results);
+      setCharacters(response.data.results);
+      })
+    .catch(err => console.log("There's an error:", err))
   }, []);
 
   return (
     <section className="character-list">
-      <h2>TODO: `array.map()` over your state here!</h2>
+      {characters.map(character => {
+        return (
+          <CharacterCard 
+            id={character.id}
+            imgSrc={character.image}
+            name={character.name}
+            status={character.status}
+            species={character.species}
+            gender={character.gender}
+            origin={character.origin.name}
+            location={character.location.name}
+          />
+        )
+      })}
     </section>
   );
 }
